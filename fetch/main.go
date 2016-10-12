8d1ab5ad02e3f71fdf595cef6aaf1d6c4ffdf9ef
tomgeudens <tom.geudens@hushmail.com>
2016-10-12 20:57:41 +0200

    added fetch program

---
+++ fetch/main.go
@@ -0,0 +1,37 @@
+/*
+description :
+  fetch prints the content found at the specified url
+
+author :
+  Tom Geudens (https://github.com/tomgeudens/)
+
+modified :
+  2016/10/12
+*/
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s", b)
+	}
+}
