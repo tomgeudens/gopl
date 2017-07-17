fba89815b00d402b0b582953af7ebaae82124156
tomgeudens <tom.geudens@hushmail.com>
2017-07-17 07:08:49 +0200

    added helloworld program

---
+++ helloworld/main.go
@@ -0,0 +1,19 @@
+/*
+description :
+  helloworld is a Go version of the Hello World program
+
+author :
+  Tom Geudens (https://github.com/tomgeudens/)
+
+modified :
+  2017/07/17
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, 世界")
+}
