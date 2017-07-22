9ac9da269998a92315738d715a9f40ef844b164d
tomgeudens <tom.geudens@hushmail.com>
2017-07-22 16:42:00 +0200

    added dup3 program

---
+++ dup3/main.go
@@ -0,0 +1,41 @@
+/*
+description :
+  dup3 prints the text of each line that appears more than once in a list
+  of named files, preceded by its count
+
+author :
+  Tom Geudens (https://github.com/tomgeudens/)
+
+modified :
+  2017/07/22
+*/
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
